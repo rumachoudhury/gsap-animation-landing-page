e768ac7cd7ea7ebc6ff251faffc09dc9e364a20e
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-05-24 15:58:42 -0400

    Updated feature section

---
+++ src/component/Boxes.jsx
@@ -0,0 +1,40 @@
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+export default function Boxes() {
+  const container = useRef();
+  const tl = useRef();
+
+  const toggleTimeline = () => {
+    tl.current.reversed(!tl.current.reversed());
+  };
+
+  useGSAP(
+    () => {
+      const boxes = gsap.utils.toArray(".box");
+
+      tl.current = gsap
+        .timeline()
+        .to(boxes[0], { x: 120, rotation: 360 })
+        .to(boxes[1], { x: -120, rotation: -360 }, "<")
+        .to(boxes[2], { y: -166 })
+        .reverse();
+    },
+    { scope: container },
+  );
+
+  return (
+    <main>
+      <section className="boxes-container" ref={container}>
+        <h2>Use the button to toggle a Timeline</h2>
+
+        <button onClick={toggleTimeline}>Toggle Timeline</button>
+
+        <div className="box gradient-blue">Box 1</div>
+        <div className="box gradient-blue">Box 2</div>
+        <div className="box gradient-blue">Box 3</div>
+      </section>
+    </main>
+  );
+}
